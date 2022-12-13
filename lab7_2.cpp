7a31337559dff8da1d2c91c23a262f0658e998bf
VODAKA <poottipong_k@cmu.ac.th>
2022-12-13 16:18:04 +0700

    lab7_2 finished

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,12 +3,33 @@
 using namespace std;
 
 int main(){
-	
-	int i = 0, P[5000], k, result;
+
+	int i = 0, P[5000], k;
+	cout << "Please input k: ";
+	cin >> k; 
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
+	if(k > 0){
+		int result = 0;
+		int z = 0;
+		while(z < 5000){
+			if(P[z]%k == 0){
+				result -= P[z];
+			
+			}else{
+				result += P[z];
+				
+			}
+			z++;
+		}
+		cout << "Result = " << result;
+		
+	}else{
+		cout << "Invalid input!!!";
+	}
 	
 	return 0;
+
 }
